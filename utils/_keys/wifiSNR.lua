d911da975a2563826c82658d52e2e41abe9d230a
asmagill <asmagill@users.noreply.github.com>
2018-08-18 02:30:32 -0500

    fixed some positioning, added wifiMeter, really need to make into a spoon someday...

--- utils/_keys/wifiSNR.lua
+++ utils/_keys/wifiSNR.lua
@@ -6,6 +6,8 @@ local screen = require("hs.screen")
 local hotkey  = require("hs.hotkey")
 local mods    = require("hs._asm.extras").mods
 
+local wifimeter = require("utils.wifimeter")
+
 local module = {}
 
 local noiseColor  = { green = 1, alpha = .5 }
@@ -22,19 +24,19 @@ local offsetFromLLy = 1
 
 -- some pre-calculations that don't change over time or with screen changes
 local scaleRatio = scaleToHeight / 120
-local screenOffsetFromLLx = sampleSize * sampleRate + padding + offsetFromLLx
-local screenOffsetFromLLy = scaleToHeight + padding + offsetFromLLy
+local screenOffsetFromLLx = sampleSize * sampleRate + padding * 2 + offsetFromLLx
+local screenOffsetFromLLy = scaleToHeight + padding * 2 + offsetFromLLy
 local boxWidth = sampleSize * sampleRate + padding * 2
 local boxHeight = scaleToHeight + padding * 2
 
 local signalPoints, noisePoints = {}, {}
 for i = 1, sampleSize, 1 do
-    table.insert(noisePoints, { x = padding + i * sampleRate, y = boxHeight - padding })
-    table.insert(signalPoints, { x = padding + i * sampleRate, y = boxHeight - padding })
+    table.insert(noisePoints, { x = padding + i * sampleRate, y = boxHeight - padding})
+    table.insert(signalPoints, { x = padding + i * sampleRate, y = boxHeight - padding})
 end
-table.insert(noisePoints, 1, { x = padding, y = boxHeight - padding })
+table.insert(noisePoints, 1, { x = padding + sampleRate, y = boxHeight - padding })
 table.insert(noisePoints, { x = padding + sampleSize * sampleRate, y = boxHeight - padding })
-table.insert(signalPoints, 1, { x = padding, y = boxHeight - padding })
+table.insert(signalPoints, 1, { x = padding + sampleRate, y = boxHeight - padding })
 table.insert(signalPoints, { x = padding + sampleSize * sampleRate, y = boxHeight - padding })
 
 local _canvas = canvas.new{ x = 0, y = 0, h = boxHeight, w = boxWidth }:behavior{"canJoinAllSpaces"}
@@ -112,11 +114,31 @@ module.stop = function()
     module.sampleTimer:stop()
 end
 
+module.wifimeter = wifimeter
+module["2GHz"] = wifimeter.new("2GHz"):setNetworkPersistence(0)
+module["5GHz"] = wifimeter.new("5GHz"):setNetworkPersistence(0)
+
 hotkey.bind(mods.CASC, "w", function()
     if module.sampleTimer:running() then
         module.stop()
+        module["2GHz"] = module["2GHz"]:hide():stop()
+        module["5GHz"] = module["5GHz"]:hide():stop()
     else
         module.start()
+        local screenFrame = screen.primaryScreen():fullFrame()
+        module["2GHz"] = module["2GHz"]:setFrame{
+            x = screenFrame.x + screenFrame.w * 1/10,
+            y = screenFrame.y + 44,
+            h = screenFrame.h * 1/3,
+            w = screenFrame.w * 8/10,
+        }:show():start()
+        module["5GHz"] = module["5GHz"]:setFrame{
+            x = screenFrame.x + screenFrame.w * 1/10,
+            y = screenFrame.y + 66 + screenFrame.h * 1/3,
+            h = screenFrame.h * 1/3,
+            w = screenFrame.w * 8/10,
+        }:show():start()
+
     end
 end)
 
