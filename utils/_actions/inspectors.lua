38494e24e05b3065b20cb0acda127070939d39b5
asmagill <asmagill@users.noreply.github.com>
2019-03-31 14:56:04 -0500

    simplify and update docs

--- utils/_actions/inspectors.lua
+++ utils/_actions/inspectors.lua
@@ -64,14 +64,15 @@ minspect = function(stuff)
 end
 
 finspect2 = function(what, ...)
-    local fn, opt = inspect, {}
+    local fn, opt = inspect, { newline = " ", indent = "" }
     for i,v in ipairs(table.pack(...)) do
         -- "inspect" is a table with a __call metamethod, so...
         local vType = (getmetatable(v) or {}).__call and "function" or type(v)
         if vType == "function" then fn = v end
         if vType == "table"    then opt = v end
     end
-    return (fn(what, opt):gsub("%s+", " "))
+--     return (fn(what, opt):gsub("%s+", " "))
+    return fn(what, opt)
 end
 
 module.help = function(...)
@@ -88,22 +89,24 @@ This module creates some shortcuts for inspecting Lua data:
                 this allows tables which contain non-numeric keys only because of
                 table.pack to be treated as the arrays they really are.
 
+    Note that a second argument to any of the `inspect*` shortcuts is appended to the
+    default table described; i.e. if you specify the same key in your options table,
+    your value will override the default.
+
     finspect(...) - inspects the arguments, first combining them into a table if more
                     then one or if the one provided is not already a table. Flattens
-                    the output by replacing run-on spaces, tabs, and newlines into a
-                    single space.
+                    the output.
 
     finspect2(what, [fn], [opt]) - inspects `what` with the function `fn` (default is
                                    "inspect") with the specfied options (default {}) and
-                                   "flattens" the output by replacing run-on spaces, tabs,
-                                   and newlines into a single space.
+                                   "flattens" the output.
+
+    minspect(what) - inspects `what`, flattening subtables, but listing each top level
+                     key on its own line.
 
     timestamp([number]) - returns the current or specified time as a string in the format
                           of 'YYYY-MM-DD hh:mm:ss.nnnn'
 
-    Note that a second argument to any of the `inspect*` shortcuts is appended to the
-    default table described; i.e. if you specify the same key in your options table,
-    your value will override the default.
 
 ]]
     return output
