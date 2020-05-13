1db947360eece5847715c189462b776a79acf824
asmagill <asmagill@users.noreply.github.com>
2020-05-13 16:50:44 -0500

    wip

--- _scratch/turtleGraphicExamples.lua
+++ _scratch/turtleGraphicExamples.lua
@@ -10,34 +10,48 @@
 -- window clearscreen pu bk 150 pd
 -- fern 25 1
 
-fern = function(i)
-    i = i or 10
-
-    local tgFern
-    tgFern = function(cv, size, sign)
+tgFern = function(cv, size, sign)
+    local tgFern2
+    tgFern2 = function(cv, size, sign)
         if size < 1 then return end
         cv:forward(size)
         cv:right(70 * sign)
-        tgFern(cv, size * .5, -sign)
+        tgFern2(cv, size * .5, -sign)
         cv:left(70 * sign)
 
         cv:forward(size)
         cv:left(70 * sign)
-        tgFern(cv, size * .5, sign)
+        tgFern2(cv, size * .5, sign)
         cv:right(70 * sign)
 
         cv:right(7 * sign)
-        tgFern(cv, size -1, sign)
+        tgFern2(cv, size -1, sign)
         cv:left(7 * sign)
 
         cv:back(size * 2)
     end
 
+    cv:penup():back(150):pendown()
+    tgFern2(cv, size, sign)
+end
+
+fern2 = function(i, s, yr)
+    i = i or 10
+
     coroutine.wrap(function()
-        local tg = dofile("_scratch/turtleGraphics.lua")
-        cv = tg.new():hideTurtle():penUp():back(150):penDown()
+        tg = require("hs.canvas.turtle")
+        cv = tg.turtleCanvas()
+
+        if yr == false then
+            cv:_neverYield(true)
+        elseif yr then
+            cv:_yieldRatio(yr)
+        else
+            cv:_yieldRatio(1000)
+        end
+
         local t = os.time()
-        tgFern(cv, i, 1)
+        tgFern(cv, i, s or 1)
         print(os.time() - t)
     end)()
 end
@@ -56,37 +70,45 @@ end
 -- clearscreen
 -- tree 150
 
-tree = function(i)
+tgTree = function(cv, size)
+    if size < 5 then
+        cv:forward(size):back(size)
+        return
+    end
+    cv:forward(size / 3)
+    cv:left(30)
+    tgTree(cv, size * 2/3)
+    cv:right(30)
+
+    cv:forward(size / 6)
+    cv:right(25)
+    tgTree(cv, size / 2)
+    cv:left(25)
+
+    cv:forward(size / 3)
+    cv:right(25)
+    tgTree(cv, size / 2)
+    cv:left(25)
+
+    cv:forward(size / 6)
+    cv:back(size)
+end
+
+tree2 = function(i, yr)
     i = i or 50
 
-    local tgTree
-    tgTree = function(cv, size)
-        if size < 5 then
-            cv:forward(size):back(size)
-            return
-        end
-        cv:forward(size / 3)
-        cv:left(30)
-        tgTree(cv, size * 2/3)
-        cv:right(30)
-
-        cv:forward(size / 6)
-        cv:right(25)
-        tgTree(cv, size / 2)
-        cv:left(25)
-
-        cv:forward(size / 3)
-        cv:right(25)
-        tgTree(cv, size / 2)
-        cv:left(25)
-
-        cv:forward(size / 6)
-        cv:back(size)
-    end
-
     coroutine.wrap(function()
-        local tg = dofile("_scratch/turtleGraphics.lua")
-        cv = tg.new():hideTurtle()
+        tg = require("hs.canvas.turtle")
+        cv = tg.turtleCanvas()
+
+        if yr == false then
+            cv:_neverYield(true)
+        elseif yr then
+            cv:_yieldRatio(yr)
+        else
+            cv:_yieldRatio(1000)
+        end
+
         local t = os.time()
         tgTree(cv, i)
         print(os.time() - t)
