6441d32434766a5edfb0519c28fea588e905143f
asmagill <asmagill@users.noreply.github.com>
2020-12-06 13:37:38 -0600

    match lsAdditions __type for registry tables

--- _scratch/drEval.lua
+++ _scratch/drEval.lua
@@ -10,24 +10,25 @@ drEval = function(l, everyThing)
                 local kvCt = 0
                 local kvIdx, _ = next(v)
                 while kvIdx do
-                    kvCt = kvCt + 1
+                    if kvIdx ~= "__type" then kvCt = kvCt + 1 end
                     kvIdx, _ = next(v, kvIdx)
                 end
 
                 local isArray, isKV = (#v > 0), (kvCt > #v)
-                if not isArray and not isKV then
-                    val = nil
-                elseif isArray and not isKV then
-                    val = string.format("array: %d items", #v)
-                elseif not isArray and isKV then
-                    val = string.format("kv:    %d pairs", kvCt)
-                else -- isArray and isKV then
-                    val = string.format("table: %d items +  %d pairs", #v, kvCt - #v)
-                end
+--                 if not isArray and not isKV then
+--                     val = nil
+--                 elseif isArray and not isKV then
+--                     val = string.format("array: %d items", #v)
+--                 elseif not isArray and isKV then
+--                     val = string.format("kv:    %d pairs", kvCt)
+--                 else -- isArray and isKV then
+--                     val = string.format("table: %d items +  %d pairs", #v, kvCt - #v)
+--                 end
+                val = string.format("%4d / %4d kv", #v, kvCt - #v)
             end
         else
             if not everyThing then val = nil end
         end
-        if val then print(i, ty, val) end
+        if val then print(i, ty, val, v.__type or "** unknown **") end
     end
 end
